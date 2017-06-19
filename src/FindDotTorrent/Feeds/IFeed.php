c3eb44fb0cf58f861b29a00612557d79c85fa578
PXgamer <owzie123@gmail.com>
2017-06-19 13:47:15 +0100

    Reformat Feeds\IFeed to PSR-2

--- src/FindDotTorrent/Feeds/IFeed.php
+++ src/FindDotTorrent/Feeds/IFeed.php
@@ -2,14 +2,19 @@
 
 namespace FindDotTorrent\Feeds;
 
-interface IFeed {
+interface IFeed
+{
     // Getters for basic information about this feed
     public function getIdentifier();
+
     public function getName();
+
     public function getEnabled();
+
     public function getUrl();
 
     // Functions used in RSS parsing
     public function makeSearchUrl($term);
+
     public function fetchResults($response);
 }
