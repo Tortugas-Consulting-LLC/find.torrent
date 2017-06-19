2387fcea3b7e17987ffae8c22c74024c58785e8d
PXgamer <owzie123@gmail.com>
2017-06-19 15:02:10 +0100

    Added fallback for non-existent cURL response

--- src/FindDotTorrent/Feeds/Mininova.php
+++ src/FindDotTorrent/Feeds/Mininova.php
@@ -19,22 +19,24 @@ class Mininova extends BaseFeed implements IFeed
 
     public function fetchResults($response)
     {
-        $dom = new \DOMDocument();
-        $dom->loadXML($response);
+        $results = [];
+        if ($response) {
+            $dom = new \DOMDocument();
+            $dom->loadXML($response);
 
-        $xmlPath = new \DOMXPath($dom);
-        $itemPath = $xmlPath->query('*/item');
+            $xmlPath = new \DOMXPath($dom);
+            $itemPath = $xmlPath->query('*/item');
 
-        $results = array();
-        foreach ($itemPath as $item) {
-            $result = new SearchResult();
-            /** @var \DOMElement $item */
-            $result->setId($item->getElementsByTagName('guid')->item(0)->nodeValue)
-                ->setTitle($item->getElementsByTagName('title')->item(0)->nodeValue)
-                ->setLink($item->getElementsByTagName('enclosure')->item(0)->getAttribute('url'))
-                ->setSource($this->getName());
+            foreach ($itemPath as $item) {
+                $result = new SearchResult();
+                /** @var \DOMElement $item */
+                $result->setId($item->getElementsByTagName('guid')->item(0)->nodeValue)
+                    ->setTitle($item->getElementsByTagName('title')->item(0)->nodeValue)
+                    ->setLink($item->getElementsByTagName('enclosure')->item(0)->getAttribute('url'))
+                    ->setSource($this->getName());
 
-            $results[] = $result;
+                $results[] = $result;
+            }
         }
 
         return $results;
