0c5009157abb4e96650ddc249aaf7b42c7c13834
PXgamer <owzie123@gmail.com>
2017-06-19 13:45:58 +0100

    Updated template spacing

--- app/templates/layouts/rels.html.php
+++ app/templates/layouts/rels.html.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>find.torrent :: <?php echo ucwords($rel); ?></title>
-  <link href="//netdna.bootstrapcdn.com/bootswatch/2.3.1/cosmo/bootstrap.min.css" rel="stylesheet" type="text/css" />
-  <link href="//yandex.st/highlightjs/7.3/styles/zenburn.min.css" rel="stylesheet" type="text/css"/>
-  <link href="../assets/styles/rels.css" rel="stylesheet" type="text/css"/>
+    <title>find.torrent :: <?php echo ucwords($rel); ?></title>
+    <link href="//netdna.bootstrapcdn.com/bootswatch/2.3.1/cosmo/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="//yandex.st/highlightjs/7.3/styles/zenburn.min.css" rel="stylesheet" type="text/css"/>
+    <link href="../assets/styles/rels.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-  <div id="container">
+<div id="container">
     <div class="navbar">
-      <div class="navbar-inner">
-        <a class="brand" href="https://github.com/Tortugas-Consulting-LLC/find.torrent">find.torrent</a>
-        <ul class="nav">
-          <li><a href="/">Root URI</a></li>
-          <li><a href="/rels/">Documentation Home</a></li>
-        </ul>
-      </div>
+        <div class="navbar-inner">
+            <a class="brand" href="https://github.com/Tortugas-Consulting-LLC/find.torrent">find.torrent</a>
+            <ul class="nav">
+                <li><a href="/">Root URI</a></li>
+                <li><a href="/rels/">Documentation Home</a></li>
+            </ul>
+        </div>
     </div>
     <h1 id="rel"><?= ucwords($rel); ?></h1>
     <?php echo \Michelf\MarkdownExtra::defaultTransform($yield); ?>
-  </div>
-  <script src="//yandex.st/highlightjs/7.3/highlight.min.js"></script>
-  <script>hljs.initHighlightingOnLoad();</script>
+</div>
+<script src="//yandex.st/highlightjs/7.3/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
 </body>
 </html>
