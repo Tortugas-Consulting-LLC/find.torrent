ad31188adb128166b0c884e7e761b8efaf1364af
PXgamer <owzie123@gmail.com>
2017-06-19 14:13:36 +0100

    Updated Models tests

--- tests/Models/BaseFeedTest.php
+++ tests/Models/BaseFeedTest.php
@@ -2,13 +2,20 @@
 
 namespace Tests\Models;
 
-class BaseFeedTest extends \Tests\BulletTestCase
+use FindDotTorrent\Feeds\BaseFeed;
+use Tests\BulletTestCase;
+
+class BaseFeedTest extends BulletTestCase
 {
+    /**
+     * @var BaseFeed
+     */
     protected $feed;
 
     public function setup()
     {
-        $this->feed = $this->getMock('\FindDotTorrent\Feeds\BaseFeed', array('makeSearchUrl', 'fetchResults', 'getUrl'));
+        $this->feed = $this->getMock('\FindDotTorrent\Feeds\BaseFeed',
+            array('makeSearchUrl', 'fetchResults', 'getUrl'));
     }
 
     public function testFeedName()
