467e1ba61068bb0660eaf04d3f83d9a691c8f05a
PXgamer <owzie123@gmail.com>
2017-06-19 14:06:03 +0100

    Added PhpDoc comments to KeyHandler and Key

--- src/FindDotTorrent/Key.php
+++ src/FindDotTorrent/Key.php
@@ -2,17 +2,37 @@
 
 namespace FindDotTorrent;
 
+/**
+ * Class Key
+ * @package FindDotTorrent
+ */
 class Key
 {
+    /**
+     * @var int
+     */
     protected $id;
+    /**
+     * @var string
+     */
     protected $public_key;
+    /**
+     * @var string
+     */
     protected $private_key;
 
+    /**
+     * @return mixed
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * @param int $id
+     * @return $this
+     */
     public function setId($id)
     {
         $this->id = $id;
@@ -20,11 +40,18 @@ class Key
         return $this;
     }
 
+    /**
+     * @return mixed
+     */
     public function getPublicKey()
     {
         return $this->public_key;
     }
 
+    /**
+     * @param string $public_key
+     * @return $this
+     */
     public function setPublicKey($public_key)
     {
         $this->public_key = $public_key;
@@ -32,11 +59,18 @@ class Key
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getPrivateKey()
     {
         return $this->private_key;
     }
 
+    /**
+     * @param string $private_key
+     * @return $this
+     */
     public function setPrivateKey($private_key)
     {
         $this->private_key = $private_key;
--- src/FindDotTorrent/KeyHandler.php
+++ src/FindDotTorrent/KeyHandler.php
@@ -2,19 +2,33 @@
 
 namespace FindDotTorrent;
 
+/**
+ * Class KeyHandler
+ * @package FindDotTorrent
+ */
 class KeyHandler
 {
+    /**
+     * @var \PDO
+     */
     protected $db;
 
+    /**
+     * KeyHandler constructor.
+     * @param $db
+     */
     public function __construct($db)
     {
         $this->db = $db;
     }
 
+    /**
+     * @return array
+     */
     public function findAll()
     {
         $keys = array();
-        $fetch_stmt = $this->db->prepare("SELECT * FROM keys");
+        $fetch_stmt = $this->db->prepare("SELECT * FROM `keys`");
         $fetch_stmt->execute();
         foreach ($fetch_stmt->fetchAll(\PDO::FETCH_ASSOC) as $row) {
             $key = new Key();
@@ -28,9 +42,13 @@ class KeyHandler
         return $keys;
     }
 
+    /**
+     * @param $id
+     * @return bool|mixed
+     */
     public function find($id)
     {
-        foreach($this->findAll() as $key) {
+        foreach ($this->findAll() as $key) {
             if ($id == $key->getId()) {
                 return $key;
             }
@@ -39,9 +57,13 @@ class KeyHandler
         return false;
     }
 
+    /**
+     * @param $public_key
+     * @return bool|mixed
+     */
     public function findByPublicKey($public_key)
     {
-        foreach($this->findAll() as $key) {
+        foreach ($this->findAll() as $key) {
             if ($public_key == $key->getPublicKey()) {
                 return $key;
             }
@@ -50,37 +72,48 @@ class KeyHandler
         return false;
     }
 
+    /**
+     * @param Key $key
+     * @throws \Exception
+     */
     public function persist(Key $key)
     {
         $statement = $this->db->prepare("
-            REPLACE INTO keys (id, public_key, private_key)
+            REPLACE INTO `keys` (id, public_key, private_key)
             VALUES (:id, :public_key, :private_key)
         ");
         $result = $statement->execute(array(
-            ":id"          => $key->getId(),
-            ":public_key"  => $key->getPublicKey(),
+            ":id" => $key->getId(),
+            ":public_key" => $key->getPublicKey(),
             ":private_key" => $key->getPrivateKey(),
         ));
 
-        if(false === $result) {
+        if (false === $result) {
             throw new \Exception(var_export($statement->errorInfo(), true));
         }
     }
 
+    /**
+     * @param Key $key
+     * @throws \Exception
+     */
     public function remove($key)
     {
         $statement = $this->db->prepare("
-            DELETE FROM keys WHERE id = :id
+            DELETE FROM `keys` WHERE id = :id
         ");
         $result = $statement->execute(array(
             ":id" => $key->getId(),
         ));
 
-        if(false === $result) {
+        if (false === $result) {
             throw new \Exception(var_export($statement->errorInfo(), true));
         }
     }
 
+    /**
+     * @return Key
+     */
     public function generate()
     {
         $key = new Key();
