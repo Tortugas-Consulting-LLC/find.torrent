52ed4e9303034b46aaa8e4ee085efa844966a73d
PXgamer <owzie123@gmail.com>
2017-06-19 14:02:10 +0100

    Added PhpDoc comments to FeedHandler

--- src/FindDotTorrent/FeedHandler.php
+++ src/FindDotTorrent/FeedHandler.php
@@ -2,25 +2,43 @@
 
 namespace FindDotTorrent;
 
+use FindDotTorrent\Feeds\BaseFeed;
+
+/**
+ * Class FeedHandler
+ * @package FindDotTorrent
+ */
 class FeedHandler
 {
+    /**
+     * @var \PDO
+     */
     protected $db;
 
+    /**
+     * FeedHandler constructor.
+     * @param $db
+     */
     public function __construct($db)
     {
         $this->db = $db;
     }
 
+    /**
+     * @return array
+     */
     public function findAll()
     {
-        $feeds        = array();
+        $feeds = array();
         $enabled_stmt = $this->db->prepare("SELECT enabled FROM feeds WHERE id = :id");
         foreach (glob(__DIR__ . "/Feeds/*.php") as $filename) {
             $feed_name = str_replace(".php", "", basename($filename));
-            if(in_array($feed_name, array('IFeed', 'BaseFeed', 'Fetch', 'SearchResult'))) {
+            if (in_array($feed_name, array('IFeed', 'BaseFeed', 'Fetch', 'SearchResult'))) {
                 continue;
             }
             $feed_name = "\\FindDotTorrent\\Feeds\\{$feed_name}";
+
+            /** @var BaseFeed $feed */
             $feed = new $feed_name;
 
             // Check to see if this feed is enabled
@@ -38,9 +56,13 @@ class FeedHandler
         return $feeds;
     }
 
+    /**
+     * @param $id
+     * @return bool|mixed
+     */
     public function find($id)
     {
-        foreach($this->findAll() as $feed) {
+        foreach ($this->findAll() as $feed) {
             if ($feed->getIdentifier() == $id) {
                 return $feed;
             }
@@ -49,15 +71,20 @@ class FeedHandler
         return false;
     }
 
+    /**
+     * @param $feed
+     * @throws \Exception
+     */
     public function persist($feed)
     {
         $statement = $this->db->prepare("REPLACE INTO feeds (id, enabled) VALUES (:id, :enabled)");
+        /** @var BaseFeed $feed */
         $result = $statement->execute(array(
-            ":id"      => $feed->getIdentifier(),
+            ":id" => $feed->getIdentifier(),
             ":enabled" => $feed->getEnabled() ? 1 : 0
         ));
 
-        if(false === $result) {
+        if (false === $result) {
             throw new \Exception(var_export($statement->errorInfo(), true));
         }
     }
